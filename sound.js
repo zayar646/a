ab51fc7f0475c47c17e522c5dcd5c55958bee8aa
zayar646 <107255507+zayar646@users.noreply.github.com>
2023-01-13 13:45:22 +0630

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,870 @@
+function searchsongs() {
+		hi.style.filter = "blur(4px)";
+		searchsong.style.display = "flex";
+}
+var s11 = document.getElementById("song11");
+var s22 = document.getElementById("song22");
+var s33 = document.getElementById("song33");
+var s44 = document.getElementById("song44");
+var	s55 = document.getElementById("song55");
+var s66 = document.getElementById("song66");
+var s77 = document.getElementById("song77");
+var s88 = document.getElementById("song88");
+var s99 = document.getElementById("song99");
+var s00 = document.getElementById("song00");
+var s12 = new Audio("../League-of-Introverts/pj song1/to the bone - pamungkas (guitar cover).mp3")
+var s13 = new Audio("../League-of-Introverts/pj song1/Pone Yape - မမြင့်.mp3")
+var s14 = new Audio("../League-of-Introverts/pj song1/ONLY - LEEHI (guitar cover).mp3")
+var s15 = new Audio("../League-of-Introverts/pj song1/life goes on - bts (guitar fingerstyle cover).mp3")
+		s11.volume = 0.5
+		s22.volume = 0.5
+		s33.volume = 0.5
+		s44.volume = 0.5
+		s55.volume = 0.5
+		s66.volume = 0.5
+		s77.volume = 0.5
+		s88.volume = 0.5
+		s99.volume = 0.5
+		s00.volume = 0.5
+		s12.volume = 0.5
+		s13.volume = 0.5
+		s14.volume = 0.5
+		s15.volume = 0.5
+function song1() {
+	b=1
+	s22.pause()
+
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s11.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing ခေါင်းလောင်းလေးတွေမြည်နေပြီ - အောင်ကိုလတ်")
+	
+}
+function song2() {
+	b=4
+	s11.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s22.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing တန်ဖိုးထားပါ့မယ် - ကောင်းကောင်း")
+}
+function song3() {
+	b=7
+	s11.pause()
+	s22.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s33.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing Yiruma, (이루마) - River Flows in You")
+}
+function song4() {
+	b=10
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s44.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing ခင်မောင်တိုး - လွမ်း")
+}
+function song5() {
+	b=13
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s55.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing ချစ်သူသိစေ - ခင်မောင်တိုး")
+}
+function song6() {
+	b=16
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s66.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing ငါ့ရဲ့လျှို့ဝှက်ချက်")
+}
+function song7() {
+	b=19
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s77.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing ဆု -ရင်ဂို")
+}
+function song8() {
+	b=22
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s88.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing မျက်သွယ် - ခင်မောင်တိုး")
+}
+function song9() {
+	a=25
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s99.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing ဝတ်မှုန် - ခင်မောင်တိုး")
+}
+function song0() {
+	b=28
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s00.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing အောင်ထက် - အချစ်တေးကဗျာ")
+}
+function song12() {
+	b=31
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		s12.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing to the bone - pamungkas (guitar cover)")
+}
+function song13() {
+	b=34
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s14.pause()
+	s15.pause()
+		s12.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing Pone Yape - မမြင့်")
+}
+function song14() {
+	b=37
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s15.pause()
+		s14.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing ONLY - LEEHI (guitar cover)")
+}
+function song15() {
+	b=40
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+		s15.play()
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("You are playing life goes on - bts (guitar fingerstyle cover)")
+}
+function No() {
+	a=1
+		searchsong.style.display = "none"
+		hi.style.filter = "none";
+}
+function start1() {
+
+	s11.pause()
+	s22.pause()
+	s33.pause()
+	s44.pause()
+	s55.pause()
+	s66.pause()
+	s77.pause()
+	s88.pause()
+	s99.pause()
+	s00.pause()
+	s12.pause()
+	s13.pause()
+	s14.pause()
+	s15.pause()
+		stop1.style.display = "inline-block"
+		start.style.display = "none"
+		alert("song pause")
+}
+function stop11() {
+	
+	b++;
+	if (b==2) {
+		s11.play()	
+	}
+	else {
+		s11.pause()
+	}
+	if (b==2) {
+		b=1
+	}
+
+
+	if (b==5) {
+		s22.play()	
+	}
+	else {
+		s22.pause()
+	}
+	if (b==5) {
+		b=4
+	}
+
+
+	if (b==8) {
+		s33.play()
+	} 
+	else {
+		s33.pause()
+	}
+	if (b==8) {
+		b=7
+	}
+
+
+	if (b==11) {
+		s44.play()	
+	}
+	else {
+		s44.pause()
+	}
+	if (b==11) {
+		b=10
+	}
+
+
+	if (b==14) {
+		s55.play()
+	}
+	else {
+		s55.pause()
+	}
+	if (b==14) {
+		b=13
+	}
+
+
+	if (b==17) {
+		s66.play()
+	}
+	else {
+		s66.pause()
+	}
+	if (b==17) {
+		b=16
+	}
+
+
+	if (b==20) {
+		s77.play()
+	}
+	else {
+		s77.pause()
+	}
+	if (b==20) {
+		b=19
+	}
+
+
+	if (b==23) {
+		s88.play()
+	}
+	else {
+		s88.pause()
+	}
+	if (b==23) {
+		b=22
+	}
+
+
+	if (b==26) {
+		s99.play()	
+	} 
+	else {
+		s99.pause()
+	}
+	if (b==26) {
+		b=25
+	}
+
+
+	if (b==29) {
+		s00.play()
+	}
+	else {
+		s00.pause()
+	}
+	if (b==29) {
+		b=28
+	}
+
+
+	if (b==32) {
+		s12.play()
+	}
+	else {
+		s12.pause()
+	}
+	if (b==32) {
+		b=31
+	}
+
+
+	if (b==35) {
+		s13.play()
+	}
+	else {
+		s13.pause()
+	}
+	if (b==35) {
+		b=34
+	}
+
+
+	if (b==38) {
+		s14.play()
+	}
+	else {
+		s14.pause()
+	}
+	if (b==38) {
+		b=37
+	}
+
+
+	if (b==41) {
+		s15.play()
+	}
+	else {
+		s15.pause()
+	}
+	if (b==41) {
+		b=40
+	}
+		start.style.display = "inline-block"
+		stop1.style.display = "none"
+		alert("song start playing play")
+}
+vol=5
+function volup(){
+	vol++;
+	if (vol==1) {
+		s11.volume = 0.1
+		s22.volume = 0.1
+		s33.volume = 0.1
+		s44.volume = 0.1
+		s55.volume = 0.1
+		s66.volume = 0.1
+		s77.volume = 0.1
+		s88.volume = 0.1
+		s99.volume = 0.1
+		s00.volume = 0.1
+		s12.volume = 0.1
+		s13.volume = 0.1
+		s14.volume = 0.1
+		s15.volume = 0.1
+	}
+	if (vol==2) {
+		s11.volume = 0.2
+		s22.volume = 0.2
+		s33.volume = 0.2
+		s44.volume = 0.2
+		s55.volume = 0.2
+		s66.volume = 0.2
+		s77.volume = 0.2
+		s88.volume = 0.2
+		s99.volume = 0.2
+		s00.volume = 0.2
+		s12.volume = 0.2
+		s13.volume = 0.2
+		s14.volume = 0.2
+		s15.volume = 0.2
+	}
+	if (vol==3) {
+		s11.volume = 0.3
+		s22.volume = 0.3
+		s33.volume = 0.3
+		s44.volume = 0.3
+		s55.volume = 0.3
+		s66.volume = 0.3
+		s77.volume = 0.3
+		s88.volume = 0.3
+		s99.volume = 0.3
+		s00.volume = 0.3
+		s12.volume = 0.3
+		s13.volume = 0.3
+		s14.volume = 0.3
+		s15.volume = 0.3
+	}
+	if (vol==4) {
+		s11.volume = 0.4
+		s22.volume = 0.4
+		s33.volume = 0.4
+		s44.volume = 0.4
+		s55.volume = 0.4
+		s66.volume = 0.4
+		s77.volume = 0.4
+		s88.volume = 0.4
+		s99.volume = 0.4
+		s00.volume = 0.4
+		s12.volume = 0.4
+		s13.volume = 0.4
+		s14.volume = 0.4
+		s15.volume = 0.4
+	}
+	if (vol==5) {
+		s11.volume = 0.5
+		s22.volume = 0.5
+		s33.volume = 0.5
+		s44.volume = 0.5
+		s55.volume = 0.5
+		s66.volume = 0.5
+		s77.volume = 0.5
+		s88.volume = 0.5
+		s99.volume = 0.5
+		s00.volume = 0.5
+		s12.volume = 0.5
+		s13.volume = 0.5
+		s14.volume = 0.5
+		s15.volume = 0.5
+	}
+	if (vol==6) {
+		s11.volume = 0.6
+		s22.volume = 0.6
+		s33.volume = 0.6
+		s44.volume = 0.6
+		s55.volume = 0.6
+		s66.volume = 0.6
+		s77.volume = 0.6
+		s88.volume = 0.6
+		s99.volume = 0.6
+		s00.volume = 0.6
+		s12.volume = 0.6
+		s13.volume = 0.6
+		s14.volume = 0.6
+		s15.volume = 0.6
+	}
+	if (vol==7) {
+		s11.volume = 0.7
+		s22.volume = 0.7
+		s33.volume = 0.7
+		s44.volume = 0.7
+		s55.volume = 0.7
+		s66.volume = 0.7
+		s77.volume = 0.7
+		s88.volume = 0.7
+		s99.volume = 0.7
+		s00.volume = 0.7
+		s12.volume = 0.7
+		s13.volume = 0.7
+		s14.volume = 0.7
+		s15.volume = 0.7
+	}
+	if (vol==8) {
+		s11.volume = 0.8
+		s22.volume = 0.8
+		s33.volume = 0.8
+		s44.volume = 0.8
+		s55.volume = 0.8
+		s66.volume = 0.8
+		s77.volume = 0.8
+		s88.volume = 0.8
+		s99.volume = 0.8
+		s00.volume = 0.8
+		s12.volume = 0.8
+		s13.volume = 0.8
+		s14.volume = 0.8
+		s15.volume = 0.8
+	}
+	if (vol==9) {
+		s11.volume = 0.9
+		s22.volume = 0.9
+		s33.volume = 0.9
+		s44.volume = 0.9
+		s55.volume = 0.9
+		s66.volume = 0.9
+		s77.volume = 0.9
+		s88.volume = 0.9
+		s99.volume = 0.9
+		s00.volume = 0.9
+		s12.volume = 0.9
+		s13.volume = 0.9
+		s14.volume = 0.9
+		s15.volume = 0.9
+	}
+	if (vol==10) {
+		s11.volume = 1
+		s22.volume = 1
+		s33.volume = 1
+		s44.volume = 1
+		s55.volume = 1
+		s66.volume = 1
+		s77.volume = 1
+		s88.volume = 1
+		s99.volume = 1
+		s00.volume = 1
+		s12.volume = 1
+		s13.volume = 1
+		s14.volume = 1
+		s15.volume = 1
+	}
+	if (vol==10) {
+		
+		alert("this is the hightest volume")
+	}
+	if (vol==11) {
+		vol=10
+		alert("this is the hightest volume")
+	}
+
+}
+function voldown() {
+	vol--;
+	if (vol==1) {
+		s11.volume = 0.1
+		s22.volume = 0.1
+		s33.volume = 0.1
+		s44.volume = 0.1
+		s55.volume = 0.1
+		s66.volume = 0.1
+		s77.volume = 0.1
+		s88.volume = 0.1
+		s99.volume = 0.1
+		s00.volume = 0.1
+		s12.volume = 0.1
+		s13.volume = 0.1
+		s14.volume = 0.1
+		s15.volume = 0.1
+	}
+	if (vol==2) {
+		s11.volume = 0.2
+		s22.volume = 0.2
+		s33.volume = 0.2
+		s44.volume = 0.2
+		s55.volume = 0.2
+		s66.volume = 0.2
+		s77.volume = 0.2
+		s88.volume = 0.2
+		s99.volume = 0.2
+		s00.volume = 0.2
+		s12.volume = 0.2
+		s13.volume = 0.2
+		s14.volume = 0.2
+		s15.volume = 0.2
+	}
+	if (vol==3) {
+		s11.volume = 0.3
+		s22.volume = 0.3
+		s33.volume = 0.3
+		s44.volume = 0.3
+		s55.volume = 0.3
+		s66.volume = 0.3
+		s77.volume = 0.3
+		s88.volume = 0.3
+		s99.volume = 0.3
+		s00.volume = 0.3
+		s12.volume = 0.3
+		s13.volume = 0.3
+		s14.volume = 0.3
+		s15.volume = 0.3
+	}
+	if (vol==4) {
+		s11.volume = 0.4
+		s22.volume = 0.4
+		s33.volume = 0.4
+		s44.volume = 0.4
+		s55.volume = 0.4
+		s66.volume = 0.4
+		s77.volume = 0.4
+		s88.volume = 0.4
+		s99.volume = 0.4
+		s00.volume = 0.4
+		s12.volume = 0.4
+		s13.volume = 0.4
+		s14.volume = 0.4
+		s15.volume = 0.4
+	}
+	if (vol==5) {
+		s11.volume = 0.5
+		s22.volume = 0.5
+		s33.volume = 0.5
+		s44.volume = 0.5
+		s55.volume = 0.5
+		s66.volume = 0.5
+		s77.volume = 0.5
+		s88.volume = 0.5
+		s99.volume = 0.5
+		s00.volume = 0.5
+		s12.volume = 0.5
+		s13.volume = 0.5
+		s14.volume = 0.5
+		s15.volume = 0.5
+	}
+	if (vol==6) {
+		s11.volume = 0.6
+		s22.volume = 0.6
+		s33.volume = 0.6
+		s44.volume = 0.6
+		s55.volume = 0.6
+		s66.volume = 0.6
+		s77.volume = 0.6
+		s88.volume = 0.6
+		s99.volume = 0.6
+		s00.volume = 0.6
+		s12.volume = 0.6
+		s13.volume = 0.6
+		s14.volume = 0.6
+		s15.volume = 0.6
+	}
+	if (vol==7) {
+		s11.volume = 0.7
+		s22.volume = 0.7
+		s33.volume = 0.7
+		s44.volume = 0.7
+		s55.volume = 0.7
+		s66.volume = 0.7
+		s77.volume = 0.7
+		s88.volume = 0.7
+		s99.volume = 0.7
+		s00.volume = 0.7
+		s12.volume = 0.7
+		s13.volume = 0.7
+		s14.volume = 0.7
+		s15.volume = 0.7
+	}
+	if (vol==8) {
+		s11.volume = 0.8
+		s22.volume = 0.8
+		s33.volume = 0.8
+		s44.volume = 0.8
+		s55.volume = 0.8
+		s66.volume = 0.8
+		s77.volume = 0.8
+		s88.volume = 0.8
+		s99.volume = 0.8
+		s00.volume = 0.8
+		s12.volume = 0.8
+		s13.volume = 0.8
+		s14.volume = 0.8
+		s15.volume = 0.8
+	}
+	if (vol==9) {
+		s11.volume = 0.9
+		s22.volume = 0.9
+		s33.volume = 0.9
+		s44.volume = 0.9
+		s55.volume = 0.9
+		s66.volume = 0.9
+		s77.volume = 0.9
+		s88.volume = 0.9
+		s99.volume = 0.9
+		s00.volume = 0.9
+		s12.volume = 0.9
+		s13.volume = 0.9
+		s14.volume = 0.9
+		s15.volume = 0.9
+	}
+	if (vol==0) {
+		s11.volume = 0
+		s22.volume = 0
+		s33.volume = 0
+		s44.volume = 0
+		s55.volume = 0
+		s66.volume = 0
+		s77.volume = 0
+		s88.volume = 0
+		s99.volume = 0
+		s00.volume = 0
+		s12.volume = 0
+		s13.volume = 0
+		s14.volume = 0
+		s15.volume = 0
+	}
+	if (vol==0) {
+		
+		alert("this is the lowest volume")
+	}
+	if (vol==-1) {
+		vol = 0
+		alert("this is the hightest volume")
+	}
+}
